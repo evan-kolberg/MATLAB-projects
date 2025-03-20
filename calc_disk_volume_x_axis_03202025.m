eca8f541adaa9b83ddcdc095aac81d3b19016163
evankolberg <evankolberg@aol.com>
2025-03-20 13:03:11 -0400

    added caps to both sides

--- calc_disk_volume_x_axis_03202025.m
+++ calc_disk_volume_x_axis_03202025.m
@@ -1,10 +1,10 @@
 
 syms x
 
-f(x) = sqrt(x);
+f(x) = sqrt(x) + 2;
 
 a = 0;
-b = 4;
+b = 8;
 
 f_num = matlabFunction(f);
 
@@ -31,11 +31,17 @@ Z_x = f_num(X_x) .* sin(Theta_x);
 figure;
 surf(X_x, Y_x, Z_x);
 hold on;
-[R_cap, Theta_cap] = meshgrid(linspace(0, f_num(b), 10), theta);
-X_cap = b * ones(size(R_cap));
-Y_cap = R_cap .* cos(Theta_cap);
-Z_cap = R_cap .* sin(Theta_cap);
-surf(X_cap, Y_cap, Z_cap);
+[R_cap_a, Theta_cap_a] = meshgrid(linspace(0, f_num(a), 10), theta);
+X_cap_a = a * ones(size(R_cap_a));
+Y_cap_a = R_cap_a .* cos(Theta_cap_a);
+Z_cap_a = R_cap_a .* sin(Theta_cap_a);
+surf(X_cap_a, Y_cap_a, Z_cap_a);
+
+[R_cap_b, Theta_cap_b] = meshgrid(linspace(0, f_num(b), 10), theta);
+X_cap_b = b * ones(size(R_cap_b));
+Y_cap_b = R_cap_b .* cos(Theta_cap_b);
+Z_cap_b = R_cap_b .* sin(Theta_cap_b);
+surf(X_cap_b, Y_cap_b, Z_cap_b);
 xlabel('x'); ylabel('y'); zlabel('z');
 title('Solid of Revolution Around X-axis');
 axis equal;
