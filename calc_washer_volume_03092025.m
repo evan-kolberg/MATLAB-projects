93ecd303b6d08d94196dfe163b280f245983476c
Evan Kolberg <evankolberg@aol.com>
2025-03-19 21:57:22 -0400

    update

--- calc_washer_volume_03092025.m
+++ calc_washer_volume_03092025.m
@@ -1,4 +1,3 @@
-
 syms x
 
 f(x) = 2*x;        % outer
@@ -6,7 +5,8 @@ g(x) = 1/4 * x^2;  % inner
 
 intersections = solve(f(x) == g(x), x);
 
-real_intersections = double(intersections(imag(intersections) == 0));
+real_intersections = double(intersections);
+real_intersections = real_intersections(imag(real_intersections) == 0);
 
 fprintf('Intersection points: ');
 fprintf('%.4f ', real_intersections);
@@ -15,7 +15,10 @@ fprintf('\n');
 a = min(real_intersections);
 b = max(real_intersections);
 
-integrand = @(x) pi * ((2*x).^2 - (1/4 * x.^2).^2);
+f_num = matlabFunction(f);
+g_num = matlabFunction(g);
+
+integrand = @(x) pi * (f_num(x).^2 - g_num(x).^2);
 
 volume = integral(integrand, a, b);
 
