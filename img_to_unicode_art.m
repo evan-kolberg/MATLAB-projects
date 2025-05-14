87ed1f7142349782fd406c8c238108fb079cc267
evankolberg <evankolberg@aol.com>
2025-05-14 08:56:31 -0400

    scaling factors are set, unicode pictures look good

--- img_to_unicode_art.m
+++ img_to_unicode_art.m
@@ -2,7 +2,8 @@
 % imread returns a 3D matrix that stores pixel color data
 % might return something like [550, 550, 3]
 % [height, width, # of color channels]
-img = imread('test_image.png');
+img = imread('iStock.jpg');
+
 
 % converts the matrix to [height, width]
 % the values in each cell represent brightness
@@ -10,9 +11,18 @@ img = imread('test_image.png');
 % 255 is white and 0 is black
 gray = rgb2gray(img);
 
+
 % resize the image to have 64 rows
-% the height will scale accordingly
-gray2 = imresize(gray,[64 NaN]);
+% the height will have 96 rows
+% since the unicode characters are much
+% taller than they are wide, you must 
+% compensate for this vertical stretch
+% with a proportional horizontal stretch
+% 4 / 2.5 = 1.6 times taller
+[H, W] = size(gray);
+% do W / H to get the horizontal stretch
+% multiply by horizontal stretches
+gray2 = imresize(gray,[1024 (1024*(W/H)*1.6)]);
 
 
 % 5 levels - 5, 4, 3, 2, clear
@@ -32,10 +42,6 @@ for row = 1:r
     for col = 1:c
         value = gray2(row, col);
 
-        disp(value)
-        fprintf('gray2(%d, %d) = %d, char_index = %d\n', row, col, value, ceil(value / 51));
-
-
         char_index = ceil(value / 51);
 
         if char_index == 0
@@ -47,17 +53,23 @@ for row = 1:r
 end
 
 
+% put each row of unicode in one concatenated cell
 unicode_str = cell(r, 1);
 for i = 1:r
     unicode_str{i} = strjoin(unicode(i, :), '');
 end
 
+
+% plot figure
 fig = uifigure('Name', 'Unicode Matrix', 'Position', [100, 100, 500, 500]);
 
 txt = uitextarea(fig, 'Position', [10, 10, 480, 480]);
-txt.FontSize = 6;
+txt.FontSize = 0.5;
 txt.FontName = 'Courier New';
 txt.Value = unicode_str;
+
+
+
 
 
 
