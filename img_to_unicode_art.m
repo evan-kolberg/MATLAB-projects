65380b0c0a166dd7a921c2153e312445a26d3923
evankolberg <evankolberg@aol.com>
2025-05-14 00:55:22 -0400

    working

--- img_to_unicode_art.m
+++ img_to_unicode_art.m
@@ -14,11 +14,50 @@ gray = rgb2gray(img);
 % the height will scale accordingly
 gray2 = imresize(gray,[64 NaN]);
 
-imshow(gray2)
+
+% 5 levels - 5, 4, 3, 2, clear
+% 255 / 5 = 51
+% ranges for each level
+% 0-51, 52-102, 103-153, 154-204, 205-255
+% for this, do ceiling division
+gradient_chars = {'█', '▓', '▒', '░', ' '};
 
 
+[r, c] = size(gray2);
+
+unicode = cell(r, c);
 
 
+for row = 1:r
+    for col = 1:c
+        value = gray2(row, col);
+
+        disp(value)
+        fprintf('gray2(%d, %d) = %d, char_index = %d\n', row, col, value, ceil(value / 51));
+
+
+        char_index = ceil(value / 51);
+
+        if char_index == 0
+            char_index = 1;
+        end
+        
+        unicode{row, col} = gradient_chars{char_index};
+    end
+end
+
+
+unicode_str = cell(r, 1);
+for i = 1:r
+    unicode_str{i} = strjoin(unicode(i, :), '');
+end
+
+fig = uifigure('Name', 'Unicode Matrix', 'Position', [100, 100, 500, 500]);
+
+txt = uitextarea(fig, 'Position', [10, 10, 480, 480]);
+txt.FontSize = 6;
+txt.FontName = 'Courier New';
+txt.Value = unicode_str;
 
 
 
